a96c1baa21a0c6118d555ab8b733d2432990dfea
Olly de Sousa <0lly@protonmail.com>
2022-10-30 11:00:52 +0000

    update styling for homepage

---
+++ frontend/src/pages/home/Home.styles.ts
@@ -0,0 +1,11 @@
+import { stylesheet } from 'typestyle';
+
+const style = stylesheet({
+    background: {
+        backgroundImage: 'linear-gradient(white, lightgrey)',
+        height: '100%',
+        width: '100%',
+    },
+});
+
+export { style };
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { UserInfo } from '../../App.types';
 import { Hello } from '../../assets/Hello';
 import { Intro } from '../../assets/Intro';
+import { style } from './Home.styles';
 
 type HomePageType = {
     user: UserInfo;
@@ -8,14 +9,14 @@ type HomePageType = {
 
 const HomePage = ({ user }: HomePageType) => {
     return (
-        <>
+        <div className={style.background}>
             <Hello />
             <Intro
                 firstName={user?.first_name}
                 currentRole={user?.employment?.['current']?.role}
                 city={user?.location?.city}
             />
-        </>
+        </div>
     );
 };
 
