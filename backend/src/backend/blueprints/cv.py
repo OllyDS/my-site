7329ad737c284b94b649347d6d16d1e0cc15452e
Olly de Sousa <0lly@protonmail.com>
2023-05-27 12:58:30 +0100

    Add a new endpoint for cv pdf download

--- backend/src/backend/blueprints/cv.py
+++ backend/src/backend/blueprints/cv.py
@@ -2,29 +2,29 @@ import json
 from typing import Dict
 
 import boto3
-from quart import Blueprint, ResponseReturnValue, request
+from quart import Blueprint, ResponseReturnValue, request, make_response
 from quart_cors import route_cors
 
 from backend.lib.api_error import APIError
-from constants import CV_FILE_NAME, CV_S3_BUCKET
+from constants import CV_PDF_FILE_NAME, CV_JSON_FILE_NAME, CV_S3_BUCKET
 
 blueprint = Blueprint("cv", __name__)
 
 
-# fetches the cv document from S3
-@blueprint.get("/cv/")
+@blueprint.get("/cv/json/")
 @route_cors(
     allow_headers=["content-type"],
     allow_methods=["GET"],
     allow_origin=["*"],
 )
-async def fetch_cv() -> ResponseReturnValue:
+async def fetch_cv_json() -> ResponseReturnValue:
+    """Returns the CV in JSON format from S3"""
     try:
         # initialise the s3 client
         s3 = boto3.resource("s3")
 
         # fetch the object from s3 using the bucket_name/file_name
-        obj = s3.Object(CV_S3_BUCKET, CV_FILE_NAME)
+        obj = s3.Object(CV_S3_BUCKET, CV_JSON_FILE_NAME)
 
         # convert the body of the response to json
         data = json.load(obj.get()["Body"])
@@ -34,9 +34,9 @@ async def fetch_cv() -> ResponseReturnValue:
         raise APIError(400, "Error fetching data from S3")
 
 
-# updates the cv document in S3
 @blueprint.post("/cv/")
-async def update_cv() -> ResponseReturnValue:
+async def update_cv_json() -> ResponseReturnValue:
+    """Updates the CV JSON document in S3"""
     try:
         # read the json data being passed as params in the post request
         file_data: Dict[str, Dict[str, str]] = await request.get_json()
@@ -45,7 +45,34 @@ async def update_cv() -> ResponseReturnValue:
         s3 = boto3.client("s3")
 
         # create a json object in s3
-        s3.put_object(Body=json.dumps(file_data), Bucket=CV_S3_BUCKET, Key=CV_FILE_NAME)
+        s3.put_object(Body=json.dumps(file_data), Bucket=CV_S3_BUCKET, Key=CV_JSON_FILE_NAME)
         return {"success": f"file uploaded to bucket {CV_S3_BUCKET} successfully"}, 200
     except:
-        raise APIError(400, f"Failed to upload file {CV_FILE_NAME} to s3")
+        raise APIError(400, f"Failed to upload file {CV_JSON_FILE_NAME} to s3")
+
+
+@blueprint.get("/cv/pdf/")
+@route_cors(
+    allow_headers=["content-type"],
+    allow_methods=["GET"],
+    allow_origin=["*"],
+)
+async def fetch_cv_pdf() -> ResponseReturnValue:
+    """Fetches the CV in PDF format"""
+    try:
+        # initialise the s3 client
+        s3 = boto3.resource("s3")
+
+        # fetch the pdf from s3 using the bucket_name/file_name
+        obj = s3.Object(CV_S3_BUCKET, CV_PDF_FILE_NAME).get()
+
+        # Create PDF from streamed data
+        response = await make_response(obj['Body'].read())
+
+        # Specify the content-type header
+        response.headers['Content-Type'] = 'application/pdf'
+
+        return response, 200
+
+    except:
+        raise APIError(400, "Error fetching PDF from S3")
--- backend/src/constants.py
+++ backend/src/constants.py
@@ -1,2 +1,3 @@
 CV_S3_BUCKET = "my-cv-ollyds"
-CV_FILE_NAME = "my_cv"
+CV_JSON_FILE_NAME = "my_cv"
+CV_PDF_FILE_NAME = "Oliver_de_Sousa_Sequeira_CV.pdf"
